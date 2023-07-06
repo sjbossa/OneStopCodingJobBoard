2d138715ec8b20c43cb688716b29080eead9a8ab
sjbossa <scott.bossard@gmail.com>
2023-07-06 15:20:35 -0500

    added connection changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose');
-//TODO: FIX CONNECTION
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/OneStopCoding'
-);
 
-module.exports = mongoose.connection;
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/OneStopCoding', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }); 
+
+  const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('MongoDB connected successfully.');
+});
+
+module.exports = db;
