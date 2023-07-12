007fb6f0187f576187ff25688855d08a62922c09
Zach Hanson <zachhansonitp@gmail.com>
2023-07-12 09:52:12 -0500

    updated photos, nav-link color

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,8 +1,8 @@
 import React from "https://cdn.skypack.dev/react@17.0.1";
 import { Link } from 'react-router-dom';
 import './index.css';
-import logoImage from '../Card/images/45.jpg';
-import bottomImage from '../Card/images/2877459.jpg';
+import logoImage from '../Card/images/nerds.jpg';
+import bottomImage from '../Card/images/laughing.jpg';
 const JobCard = ({ jobs, title }) => {
   if (!jobs.length) {
     return <h3>No jobs have been posted yet.</h3>;
@@ -16,10 +16,11 @@ const JobCard = ({ jobs, title }) => {
           
           
         img={logoImage}
+        title={job.title}
+          description={job.description}
           key={job._id}
           id={job._id}
-          title={job.title}
-          description={job.description}
+         
         />
       ))}
 
@@ -39,8 +40,8 @@ function Card(props) {
       <div className="card__body">
         <img className="card__image" src={img} alt="" />
         <h2 className="card__title">{title}</h2>
-        <p className="card__id">{id}</p>
         <p className="card__description">{description}</p>
+        <p className="card__id">{id}</p>
       </div>
       <Link to={`/job/${id}`} className="card__btn">
         View Job
